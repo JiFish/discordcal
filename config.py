92e5ba7f0f9ffbcd6ce37efdb3ee08900e95cbb3
JiFish <joe@jifish.co.uk>
2025-04-13 01:22:47 +0100

    Additional configs

--- config.py
+++ config.py
@@ -17,6 +17,9 @@ DAYS_AHEAD = 14                        # Number of days to look ahead for events
 UPDATE_FREQUENCY_MINUTES = 60          # Frequency of updates in minutes
 ENABLE_STATUS_UPDATE = True            # Enable or disable updating the bot's status
 SERVER_TZ = 'Europe/London'            # Your server's timezone as a string, used for the bot's status
+EVENT_GRACE_TIME = 5                   # Number of minutes before an event starts when it will no longer be modified
 
 # Directory for event images
 IMAGE_DIRECTORY = 'images'
+# File to store event mappings
+EVENT_MAPPING_FILE = "event_mapping.json"
